fd182d3b3712bc354c232523d89dc50bb64619f3
SHAREEF SHAIK <shareef.08.2005@gmail.com>
2025-07-10 14:40:46 +0530

    Backend Auth completed!

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+// const express = require("express"); // if "type" : "commonJs" in package.json file
+
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+import authRoutes from "./routes/auth.route.js"
+
+import {connectDB} from "./lib/db.js"; 
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //allows you to parse the body of the request
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT,() => {
+    console.log("Server is running at http://localhost:" + PORT);
+    connectDB();
+})
+
+
+// b09Gm5sn5JHovKZ7
