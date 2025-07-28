69e2788f8f8218e48d224df75eddc54f9acaddbc
SHAREEF SHAIK <shareef.08.2005@gmail.com>
2025-07-28 15:14:22 +0530

    Added payments pages using stripe

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -4,12 +4,13 @@ import { motion } from "framer-motion";
 import { useCartStore } from "../stores/useCartStore";
 import { Link } from "react-router-dom";
 import { MoveRight } from "lucide-react";
-// import { loadStripe } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
-// const stripePromise = loadStripe(
-// 	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
-// );
+
+const stripePromise = loadStripe(
+	"pk_test_51RpQ1DEIZj4vCt5g44xjE0udszD38sWKKQVVjNaBYq39IYYPV3kINE4DtLO9oU7h3gYlv9Sm4zpnwZGXPxCIghpK00LyZBnV5D"
+);
 
 const OrderSummary = () => {
 	const { total, subtotal, coupon, isCouponApplied, cart } = useCartStore();
