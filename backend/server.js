c0541e35f799786375f40aa288c96f32a41490b8
SHAREEF SHAIK <shareef.08.2005@gmail.com>
2025-07-29 15:24:00 +0530

    Prepare for production deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-// const express = require("express"); // if "type" : "commonJs" in package.json file
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -17,12 +15,10 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); //allows you to parse the body of the request
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -32,18 +28,16 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// console.log(__dirname);
+// ✅ Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-// app.get("*", (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// });
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-  console.log("Server is running at" + PORT);
+  console.log("Server is running at " + PORT);
   connectDB();
 });
